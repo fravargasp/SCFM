6eeb50345bcb3b7c21614c0d758ae42c17915495
fravargasp <50473445+fravargasp@users.noreply.github.com>
2019-05-25 00:04:19 -0500

    Add files via upload

--- app/index.js
+++ app/index.js
@@ -1,23 +1,22 @@
 let document = require("document");
 import { HeartRateSensor } from "heart-rate";
 
-// Fetch UI elements we will need to change
 let hrLabel = document.getElementById("hrm");
 let updatedLabel = document.getElementById("updated");
 
-// Initialize the UI with some values
+// Se inicializan las variables de el entorno del ususario
 hrLabel.text = "--";
 updatedLabel.text = "UDFJC";
 
-// Create a new instance of the HeartRateSensor object
+// Se crea la instancia del objeto HeartRateSensor
 var hrm = new HeartRateSensor();
 
-// Declare an event handler that will be called every time a new HR value is received.
+// Se declara el controlador de eventos 
 hrm.onreading = function() {
-  // Peek the current sensor values
-  console.log("Current heart rate: " + hrm.heartRate);
+  // Ver los valores del sensor
+  console.log("Frecuencia cardiaca actual: " + hrm.heartRate);
   hrLabel.text = hrm.heartRate;
 }
 
-// Begin monitoring the sensor
+// Iniciar el monitor
 hrm.start();
